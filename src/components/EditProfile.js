308be0d5d73fe41bd100520811e7115e4d23a015
ericb2020 <ebrauer83@gmail.com>
2021-03-11 12:30:57 -0600

    able to edit User info from frontend to backend

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,45 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const EditProfile = (props) => {
-   
-    const foundUser = props.user.find(user => {
-        return user.id === parseInt(props.match.params.id);
-    })
-    // const { username, firstName, lastName, password } = this.state;
-    return (
-        <div>
-            {foundUser ? (
-                <div>
-                    <h1>Profile Page</h1>
+class EditProfile extends Component {
+    constructor(props) {
+        super(props)
+        const foundUser = this.props.user.find(user => {
+            return user.id === parseInt(this.props.match.params.id);
+        })
+        this.state = {
+            username: foundUser.username,
+            firstName: foundUser.firstName,
+            lastName: foundUser.lastName,
+            password: foundUser.password
+        }
+    }
+    
+    handleChange = (evt) => {
+        console.log(evt.target);
+        const { name, value } = evt.target;
+        this.setState({
+            [name]: value
+        })
+        
+    };
+
+    editProfile = async (e) => {
+        e.preventDefault();
+        // if(this.state.username == ""){
+        //     this.setState({
+        //         username: "something here"
+        //     });
+        // }
+
+        const data = {
+            lastName: this.state.lastName,
+            password: this.state.password
+        }
+        console.log(data);
+//////////     WHAT'S GOING ON HERE????    /////////////
+        const response = await axios.put(`http://localhost:3001/users/1`, data);
+        console.log(response)
+    }
+
+    render() {
+        const foundUser = this.props.user.find(user => {
+            return user.id === parseInt(this.props.match.params.id);
+        })
+        const { username, firstName, lastName, password } = this.state;
+        return (
+            <div>
+                {foundUser ? (
                     <div>
-                        {/* <form>
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder={foundUser.username}
-                                value="username"
-                                // onChange={this.handleChange}
-                            />
-                        </form> */}
-                            <li>username: {foundUser.username}</li>
-                            <li>firstName: {foundUser.firstName}</li>
-                            <li>lastName: {foundUser.lastName}</li>
-                            <li>password: {foundUser.password}</li>
+                        <h1>Edit Profile Page</h1>
+                        <div>
+                            <form onSubmit={this.editProfile}>
+                                First Name: <input
+                                    type="text"
+                                    name="firstName"
+                                    placeholder={foundUser.firstName}
+                                    value={ firstName }
+                                    onChange={this.handleChange}
+                                /> <br />
+                                Last Name: <input
+                                    type="text"
+                                    name="lastName"
+                                    placeholder={foundUser.lastName}
+                                    value={ lastName }
+                                    onChange={this.handleChange}
+                                /> <br />
+                                Username: <input
+                                    type="text"
+                                    name="username"
+                                    placeholder={foundUser.username}
+                                    value={ username }
+                                    onChange={this.handleChange}
+                                /> <br />
+                                Password: <input
+                                    type="text"
+                                    name="password"
+                                    placeholder={foundUser.password}
+                                    value={ password }
+                                    onChange={this.handleChange}
+                                /> <br /> <br />
+                                <input type="submit" value="Save Changes" />
+                            </form>
+                        </div>
 
                     </div>
-                    <div>
-                        <Link to='/profile/:id/edit'>
-                            <button>Edit User</button>
-                        </Link>
-                        <button>Delete User</button>
-                    </div>
-                </div>
-            ) :
-                <p>no user data found</p>
-            }
-        </div>
-    )
+                ) :
+                    <p>no user data found</p>
+                }
+            </div>
+        )
+    }
 }
 
 export default EditProfile;
